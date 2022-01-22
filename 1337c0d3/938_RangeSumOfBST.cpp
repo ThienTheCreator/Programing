c6a6f1bc0c89689d401b3ea9656058b4b27bf8eb
thien pham <47543920+ThienTheCreator@users.noreply.github.com>
2022-01-22 00:04:52 -0800

    commented 938

--- 1337c0d3/938_RangeSumOfBST.cpp
+++ 1337c0d3/938_RangeSumOfBST.cpp
@@ -1,3 +1,33 @@
+/* 938. Range Sum of BST
+
+Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+
+ 
+
+Example 1:
+
+
+Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+Output: 32
+Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
+
+Example 2:
+
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+Output: 23
+Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 2 * 104].
+1 <= Node.val <= 105
+1 <= low <= high <= 105
+All Node.val are unique.
+
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -17,11 +47,11 @@ public:
             res += root->val;
         }
         
-        if(root->left != nullptr){
+        if(root->left != nullptr && root->val > low){
             res += rangeSumBST(root->left,low,high);
         }
         
-        if(root->right != nullptr){
+        if(root->right != nullptr && root->val < high){
             res += rangeSumBST(root->right,low,high);
         }
         
@@ -31,6 +61,8 @@ public:
 
 /* Note
 
-Worked on a recursive approach.
+Worked on a recursive approach. If the root value is greater than low value
+then check left child. If the root value is less than high value then check
+the right child. Sum the value if it is between high and low.
 
 */
