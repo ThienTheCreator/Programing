b11676e737557ffa7986b23f1e2d6e1e60a1c7c4
thien pham <47543920+ThienTheCreator@users.noreply.github.com>
2022-02-01 23:53:13 -0800

    worked on 1656

---
+++ 1337c0d3/1656_DesignAnOrderedStream.cpp
@@ -0,0 +1,84 @@
+/* 1656. Design an Ordered Stream
+
+There is a stream of n (idKey, value) pairs arriving in an arbitrary order, 
+where idKey is an integer between 1 and n and value is a string. No two pairs 
+have the same id.
+
+Design a stream that returns the values in increasing order of their IDs by 
+returning a chunk (list) of values after each insertion. The concatenation of 
+all the chunks should result in a list of the sorted values.
+
+Implement the OrderedStream class:
+
+OrderedStream(int n) Constructs the stream to take n values.
+String[] insert(int idKey, String value) Inserts the pair (idKey, value) into 
+the stream, then returns the largest possible chunk of currently inserted 
+values that appear next in the order.
+ 
+
+Example:
+
+
+
+Input
+["OrderedStream", "insert", "insert", "insert", "insert", "insert"]
+[[5], [3, "ccccc"], [1, "aaaaa"], [2, "bbbbb"], [5, "eeeee"], [4, "ddddd"]]
+Output
+[null, [], ["aaaaa"], ["bbbbb", "ccccc"], [], ["ddddd", "eeeee"]]
+
+Explanation
+// Note that the values ordered by ID is ["aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"].
+OrderedStream os = new OrderedStream(5);
+os.insert(3, "ccccc"); // Inserts (3, "ccccc"), returns [].
+os.insert(1, "aaaaa"); // Inserts (1, "aaaaa"), returns ["aaaaa"].
+os.insert(2, "bbbbb"); // Inserts (2, "bbbbb"), returns ["bbbbb", "ccccc"].
+os.insert(5, "eeeee"); // Inserts (5, "eeeee"), returns [].
+os.insert(4, "ddddd"); // Inserts (4, "ddddd"), returns ["ddddd", "eeeee"].
+// Concatentating all the chunks returned:
+// [] + ["aaaaa"] + ["bbbbb", "ccccc"] + [] + ["ddddd", "eeeee"] = 
+["aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"]
+// The resulting order is the same as the order above.
+ 
+
+Constraints:
+
+1 <= n <= 1000
+1 <= id <= n
+value.length == 5
+value consists only of lowercase letters.
+Each call to insert will have a unique id.
+Exactly n calls will be made to insert.
+
+*/
+class OrderedStream {
+public:
+    vector<string> vec;
+    int index = 0;
+    OrderedStream(int n) {
+        vec.resize(n+1);
+    }
+    
+    vector<string> insert(int idKey, string value) {
+        vec[idKey-1] = value;
+        vector<string> tempVec;
+        while(vec[index] != ""){
+            tempVec.push_back(vec[index++]);
+        }
+        return tempVec;
+    }
+};
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * OrderedStream* obj = new OrderedStream(n);
+ * vector<string> param_1 = obj->insert(idKey,value);
+ */
+
+/* Note
+
+I already did this problem when I some time before. I change some of the code
+to match the previous solution after I re did the solution. Index is use to add
+values when the values can be add consecutively and then we increment it. 
+We use a vector to store the values and return a temporary vector. 
+
+*/
