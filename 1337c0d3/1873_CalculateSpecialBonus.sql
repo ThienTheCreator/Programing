18be4c43ff752454103ced799850ce8714cb10af
thien pham <47543920+ThienTheCreator@users.noreply.github.com>
2022-03-28 23:29:08 -0700

    1393 solution

--- 1337c0d3/1873_CalculateSpecialBonus.sql
+++ 1337c0d3/1873_CalculateSpecialBonus.sql
@@ -56,3 +56,9 @@ employee_id,
 IF(employee_id % 2 = 1 AND LEFT(name, 1) != "M", salary, 0) as bonus
 FROM
 Employees;
+
+/* Note 
+
+IF is like a conditional statement, and left gets how many characters from the left.
+
+*/
