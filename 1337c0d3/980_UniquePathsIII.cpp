71a1228d01bdc6daf14590d178a3eae3c98bf48b
thien pham <47543920+ThienTheCreator@users.noreply.github.com>
2022-04-15 00:00:16 -0700

    working on 980

---
+++ 1337c0d3/980_UniquePathsIII.cpp
@@ -0,0 +1,80 @@
+/* 980. Unique Paths III
+
+You are given an m x n integer array grid where grid[i][j] could be:
+
+1 representing the starting square. There is exactly one starting square.
+2 representing the ending square. There is exactly one ending square.
+0 representing empty squares we can walk over.
+-1 representing obstacles that we cannot walk over.
+Return the number of 4-directional walks from the starting square to the ending square, that walk
+over every non-obstacle square exactly once.
+
+Example 1:
+
+Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+Output: 2
+Explanation: We have the following two paths: 
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
+
+Example 2:
+
+Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+Output: 4
+Explanation: We have the following four paths: 
+1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
+
+Example 3:
+
+Input: grid = [[0,1],[2,0]]
+Output: 0
+Explanation: There is no path that walks over every empty square exactly once.
+Note that the starting and ending square can be anywhere in the grid.
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 20
+1 <= m * n <= 20
+-1 <= grid[i][j] <= 2
+There is exactly one starting cell and one ending cell.
+
+*/
+
+class Solution {
+public:
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size()));
+        
+        pair<int, int> start;
+        pair<int, int> end;
+        
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[i].size(); j++){
+                if(grid[i][j] == 1){
+                    start = make_pair(i,j);
+                }
+                if(grid[i][j] == 2){
+                    end = make_pair(i,j);
+                }           
+            }
+        }
+        
+        stack<pair<int, int>> s;
+        stack.push<start>
+        while(!s.empty()){
+            
+        }
+    }
+};
+
+/* Note
+
+Trying to implement a iterative bfs.
+I look at the solution and it looks like we have to do DFS. 
+
+*/
