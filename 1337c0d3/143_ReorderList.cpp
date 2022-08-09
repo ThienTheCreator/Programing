a32075299679437e5063bea8b8a306d2b6138202
thien pham <47543920+ThienTheCreator@users.noreply.github.com>
2022-08-08 19:06:15 -0700

    solution to 143

--- 1337c0d3/143_ReorderList.cpp
+++ 1337c0d3/143_ReorderList.cpp
@@ -73,4 +73,43 @@ public:
 The solution uses two pointer to find the middle of the linked list. Then add the second half of
 the list into a stack. Then reorder the list based on the linked list and the stack.
 
+The fastest solution which ran is below.
+
+class Solution {
+public:
+    ListNode* reverse(ListNode *head){
+        ListNode *prev=NULL;
+        ListNode *nxt;
+        ListNode *curr=head;
+        while(curr!=NULL){
+            nxt=curr->next;
+            curr->next = prev;
+            prev=curr;
+            curr=nxt;
+        }
+        return prev;
+    }
+    
+    void reorderList(ListNode* head) {
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        
+        ListNode *first=head;
+        ListNode *second = reverse(slow->next);
+        slow->next=NULL;
+        while(second){
+            ListNode *temp1 = first->next;
+            ListNode *temp2 = second->next;
+            first->next=second;
+            second->next=temp1;
+            first=temp1;
+            second=temp2;
+        }
+    }
+};
+
 */
